1a57652ee6d323d2797503e0f114e6ae44b5f01b
Daniel Ayala <danielayala1793@hotmail.com>
2025-08-30 10:56:33 -0500

    Añadido código a site-header.tsx 30/8/2025

--- resources/js/components/site-header.tsx
+++ resources/js/components/site-header.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { SidebarIcon } from "lucide-react"
-
+import AppearanceDropdown from "@/components/appearance-dropdown";
 import { SearchForm } from "@/components/search-form"
 import {
   Breadcrumb,
@@ -14,8 +14,15 @@ import {
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { useSidebar } from "@/components/ui/sidebar"
+import React from "react";
+import { AuthenticatedLayoutProps } from "@/types/layout";
 
-export function SiteHeader() {
+export function SiteHeader({
+    header,
+    children,
+    breadcrumbs = [], // Asigna un valor por defecto para evitar errores,
+    ...rest
+}: AuthenticatedLayoutProps) {
   const { toggleSidebar } = useSidebar()
 
   return (
@@ -32,18 +39,36 @@ export function SiteHeader() {
         <Separator orientation="vertical" className="mr-2 h-4" />
         <Breadcrumb className="hidden sm:block">
           <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#">
-                Building Your Application
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-            </BreadcrumbItem>
+            {breadcrumbs.map((item, index) => (
+                <React.Fragment key={index}>
+                    <BreadcrumbItem>
+                        {/* El último ítem no tiene link */}
+                        {item.href ? (
+                            <BreadcrumbLink href={item.href}>
+                                {item.label}
+                            </BreadcrumbLink>
+                        ) : (
+                            <BreadcrumbPage>{item.label}</BreadcrumbPage>
+                        )}
+                    </BreadcrumbItem>
+                    {/* Evita renderizar el separador después del último ítem */}
+                    {index < breadcrumbs.length - 1 && (
+                        <BreadcrumbSeparator />
+                    )}
+                </React.Fragment>
+            ))}
           </BreadcrumbList>
         </Breadcrumb>
-        <SearchForm className="w-full sm:ml-auto sm:w-auto" />
+
+        <div className="flex flex-1 justify-end items-center gap-4">
+          <div>
+            <AppearanceDropdown className="w-full sm:ml-auto sm:w-auto" />
+          </div>
+          <div>
+            <SearchForm className="w-full sm:ml-auto sm:w-auto" />
+          </div>
+        </div>
+
       </div>
     </header>
   )
