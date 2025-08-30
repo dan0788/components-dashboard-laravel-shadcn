d9c7975a5c68f6369e7f9c86a0bc3d2eda00fe08
Daniel Ayala <danielayala1793@hotmail.com>
2025-08-30 10:54:22 -0500

    Revert "Añadido código a appearance-dropdown.tsx 30/8/2025"
    This reverts commit 60b41f046a75d62de089c5cb2334bb0a1255a143.

--- resources/js/components/site-header.tsx
+++ resources/js/components/site-header.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { SidebarIcon } from "lucide-react"
-import AppearanceDropdown from "@/components/appearance-dropdown";
+
 import { SearchForm } from "@/components/search-form"
 import {
   Breadcrumb,
@@ -14,15 +14,8 @@ import {
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { useSidebar } from "@/components/ui/sidebar"
-import React from "react";
-import { AuthenticatedLayoutProps } from "@/types/layout";
 
-export function SiteHeader({
-    header,
-    children,
-    breadcrumbs = [], // Asigna un valor por defecto para evitar errores,
-    ...rest
-}: AuthenticatedLayoutProps) {
+export function SiteHeader() {
   const { toggleSidebar } = useSidebar()
 
   return (
@@ -39,36 +32,18 @@ export function SiteHeader({
         <Separator orientation="vertical" className="mr-2 h-4" />
         <Breadcrumb className="hidden sm:block">
           <BreadcrumbList>
-            {breadcrumbs.map((item, index) => (
-                <React.Fragment key={index}>
-                    <BreadcrumbItem>
-                        {/* El último ítem no tiene link */}
-                        {item.href ? (
-                            <BreadcrumbLink href={item.href}>
-                                {item.label}
-                            </BreadcrumbLink>
-                        ) : (
-                            <BreadcrumbPage>{item.label}</BreadcrumbPage>
-                        )}
-                    </BreadcrumbItem>
-                    {/* Evita renderizar el separador después del último ítem */}
-                    {index < breadcrumbs.length - 1 && (
-                        <BreadcrumbSeparator />
-                    )}
-                </React.Fragment>
-            ))}
+            <BreadcrumbItem>
+              <BreadcrumbLink href="#">
+                Building Your Application
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+            </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
-
-        <div className="flex flex-1 justify-end items-center gap-4">
-          <div>
-            <AppearanceDropdown className="w-full sm:ml-auto sm:w-auto" />
-          </div>
-          <div>
-            <SearchForm className="w-full sm:ml-auto sm:w-auto" />
-          </div>
-        </div>
-
+        <SearchForm className="w-full sm:ml-auto sm:w-auto" />
       </div>
     </header>
   )
