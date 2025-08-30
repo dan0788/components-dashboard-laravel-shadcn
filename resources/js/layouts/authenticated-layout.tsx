ac4360dcfd83ca78d91ad3381fce4725d5a24ca1
Daniel Ayala <danielayala1793@hotmail.com>
2025-08-30 10:58:03 -0500

    Añadido código a authenticated-layout.tsx 30/8/2025

--- resources/js/layouts/authenticated-layout.tsx
+++ resources/js/layouts/authenticated-layout.tsx
@@ -1,46 +1,29 @@
-import { PropsWithChildren, ReactNode } from "react";
-import {AppSidebar} from "@/components/app-sidebar";
-import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
-import {Separator} from "@/components/ui/separator";
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbList, BreadcrumbPage,
-} from "@/components/ui/breadcrumb";
-import AppearanceDropdown from "@/components/appearance-dropdown";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { AuthenticatedLayoutProps } from "@/types/layout";
 
 export default function AuthenticatedLayout({
     header,
-    children
-}: PropsWithChildren<{
-    header?: ReactNode;
-}>) {
+    children,
+    breadcrumbs = [], // Asigna un valor por defecto para evitar errores,
+    ...rest
+}: AuthenticatedLayoutProps) {
     return (
-        <SidebarProvider>
-            <AppSidebar />
 
-            <SidebarInset>
-                <header className="sticky top-0 bg-background flex h-16 shrink-0 items-center gap-2 justify-between p-4 border-b md:border-none md:rounded-xl">
-                    <div className="flex items-center gap-2">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator orientation="vertical" className="mr-2 h-4" />
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>{header}</BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
-                    </div>
-                    <div>
-                        <AppearanceDropdown />
-                    </div>
-                </header>
+        <div className="[--header-height:calc(theme(spacing.14))]">
+            <SidebarProvider className="flex flex-col">
+                <SiteHeader breadcrumbs={breadcrumbs} />
+                <div className="flex flex-1">
+                    <AppSidebar />
+                    <SidebarInset>
+                        <div className="flex flex-1 flex-col gap-4 p-4">
+                            {children}
+                        </div>
+                    </SidebarInset>
+                </div>
+            </SidebarProvider>
+        </div>
 
-                <main className="p-4 md:pt-0 h-full">
-                    {children}
-                </main>
-            </SidebarInset>
-        </SidebarProvider>
     );
 }
