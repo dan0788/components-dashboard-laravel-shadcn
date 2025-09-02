984efd0ea692ddeef419dc753fe3505bb655d739
Daniel Ayala <danielayala1793@hotmail.com>
2025-09-02 00:56:38 -0500

    Añadida dependencia npm install @tanstack/react-table 2/9/2025

---
+++ resources/js/pages/components/data-table.tsx
@@ -0,0 +1,24 @@
+import AuthenticatedLayout from "@/layouts/authenticated-layout";
+import { Head } from "@inertiajs/react";
+
+import { usePageData } from "@/hooks/get-page"
+
+export default function Page() {
+  const pageData = usePageData();
+
+  return (
+    <AuthenticatedLayout breadcrumbs={pageData.breadcrumbs}>
+      <Head title={pageData.title} />
+      <div className="flex flex-col items-center justify-center p-8">
+        <h1 className="text-3xl font-bold mb-8">Component: {pageData.title}</h1>
+        <div className="flex justify-center w-full max-w-xl mx-auto rounded-lg shadow-md bg-card text-card-foreground p-6">
+
+          
+
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  );
+}
+
+
