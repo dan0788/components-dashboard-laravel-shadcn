b7bc5807274311bb6ac25931c859cc089ab887e1
Daniel Ayala <danielayala1793@hotmail.com>
2025-09-06 17:57:57 -0500

    Actualización datepicker a formulario 6/9/2025

--- resources/js/pages/profile/edit.tsx
+++ resources/js/pages/profile/edit.tsx
@@ -60,7 +60,7 @@ const FormSchema = z.object({
   })/* .email({
         message: "Invalid email address.",
     }) */,
-  dateofbirth: z.string()
+  dateofbirth: z.date().optional(),
 })
 
 export default function Edit({
@@ -69,14 +69,13 @@ export default function Edit({
 }: { mustVerifyEmail: boolean; status?: string }) {
   const pageData = usePageData();
   const [open, setOpen] = React.useState(false)
-  const [date, setDate] = React.useState<Date | undefined>(undefined)
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       firstname: "",
       lastname: "",
-      dateofbirth: "",
+      dateofbirth: undefined,
     },
   })
 
@@ -156,52 +155,38 @@ export default function Edit({
                           )}
                         />
 
-                        {/* campo lastname */}
+                        {/* campo dateofbirth */}
                         <FormField
                           control={form.control}
                           name="dateofbirth"
                           render={({ field }) => (
                             <FormItem>
+                              <FormLabel className="pl-1 text-text">Date of birth</FormLabel>
                               <FormControl>
-
-                                <div className="flex flex-col gap-3">
-
-                                  <Label htmlFor="dateofbirth" className="px-1 text-text">
-                                    Date of birth
-                                  </Label>
-
-                                  <Popover open={open} onOpenChange={setOpen}>
-                                    <PopoverTrigger asChild>
-                                      <Tooltip>
-                                        <TooltipTrigger asChild>
-                                          <Button
-                                            variant="outline"
-                                            id="dateofbirth"
-                                            className="w-48 justify-between font-normal"
-                                          >
-                                            {date ? date.toLocaleDateString() : "Select date"}
-                                            <ChevronDownIcon />
-                                          </Button>
-                                        </TooltipTrigger>
-                                        <TooltipContent>
-                                          <p>This is your email.</p>
-                                        </TooltipContent>
-                                      </Tooltip>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-auto overflow-hidden p-0" align="start">
-                                      <Calendar
-                                        mode="single"
-                                        selected={date}
-                                        captionLayout="dropdown"
-                                        onSelect={(date) => {
-                                          setDate(date)
-                                          setOpen(false)
-                                        }}
-                                      />
-                                    </PopoverContent>
-                                  </Popover>
-                                </div>
-
+                                <Popover>
+                                  <PopoverTrigger asChild>
+                                    <Button
+                                      variant={"outline"}
+                                      className={"w-48 justify-between font-normal"}
+                                    >
+                                      {field.value ? (
+                                        field.value.toLocaleDateString()
+                                      ) : (
+                                        <span className="text-muted-foreground">Select date</span>
+                                      )}
+                                      <ChevronDownIcon />
+                                    </Button>
+                                  </PopoverTrigger>
+                                  <PopoverContent className="w-auto p-0" align="start">
+                                    <Calendar
+                                      mode="single"
+                                      selected={field.value}
+                                      onSelect={field.onChange} // 💡 Conecta el cambio del calendario al formulario
+                                      initialFocus
+                                      captionLayout="dropdown"
+                                    />
+                                  </PopoverContent>
+                                </Popover>
                               </FormControl>
                               <FormMessage />
                             </FormItem>
