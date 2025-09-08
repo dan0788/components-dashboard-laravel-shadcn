7039c65aa5d6d24db2479443b605f4f2727ec817
Daniel Ayala <danielayala1793@hotmail.com>
2025-09-08 15:23:30 -0500

    Añadido modelo y controlador para Pin 8/9/2025

--- app/Http/Controllers/PinController.php
+++ app/Http/Controllers/PinController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pin;
 use Illuminate\Http\Request;
 
 class PinController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pin $pin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pin $pin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pin $pin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pin $pin)
+    {
+        //
+    }
 }
